a97685c2896bffee30fbabff458344deed779a1c
Alexandre Roux Tekartik <alex@tekartik.com>
2018-10-08 15:44:24 +0200

    fix: flutter cmd

--- tool/test_flutter.dart
+++ tool/test_flutter.dart
@@ -3,29 +3,18 @@ import 'package:tekartik_build_utils/common_import.dart';
 
 Future testFlutterDir(String dir, {List<String> analyzerDir}) async {
   analyzerDir ??= ['lib'];
-  await runCmd(flutterCmd(['packages', 'get'])..workingDirectory = dir);
+  await runCmd(flutterCmd(['packages', 'get'])
+    ..runInShell = true
+    ..workingDirectory = dir);
   // '--fatal-warnings', '--fatal-infos'
-  await runCmd(
-      flutterCmd(['analyze']..addAll(analyzerDir))..workingDirectory = dir);
-  await runCmd(flutterCmd(['test'])..workingDirectory = dir);
+  await runCmd(flutterCmd(['analyze']..addAll(analyzerDir))
+    ..runInShell = true
+    ..workingDirectory = dir);
+  await runCmd(flutterCmd(['test'])
+    ..runInShell = true
+    ..workingDirectory = dir);
 }
 
 Future main() async {
-  await runCmd(dartCmd(['--version']));
-  try {
-    await runCmd(flutterCmd(['--version']));
-  } catch (e) {
-    print(e);
-  }
-  try {
-    await runCmd(ProcessCmd('flutter', ['--version']));
-  } catch (e) {
-    print(e);
-  }
-  try {
-    await runCmd(ProcessCmd('flutter', ['--version'], runInShell: true));
-  } catch (e) {
-    print(e);
-  }
   await testFlutterDir(null, analyzerDir: ['lib', 'test']);
 }
