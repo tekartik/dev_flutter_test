b15deb1c085c838c3a14aa35dca4f08e65355281
Alexandre Roux <alex@tekartik.com>
2018-10-08 16:30:39 +0200

    fix: flutter cmd

--- tool/test_flutter.dart
+++ tool/test_flutter.dart
@@ -3,16 +3,11 @@ import 'package:tekartik_build_utils/common_import.dart';
 
 Future testFlutterDir(String dir, {List<String> analyzerDir}) async {
   analyzerDir ??= ['lib'];
-  await runCmd(flutterCmd(['packages', 'get'])
-    ..runInShell = true
-    ..workingDirectory = dir);
+  await runCmd(FlutterCmd(['packages', 'get'])..workingDirectory = dir);
   // '--fatal-warnings', '--fatal-infos'
-  await runCmd(flutterCmd(['analyze']..addAll(analyzerDir))
-    ..runInShell = true
-    ..workingDirectory = dir);
-  await runCmd(flutterCmd(['test'])
-    ..runInShell = true
-    ..workingDirectory = dir);
+  await runCmd(
+      FlutterCmd(['analyze']..addAll(analyzerDir))..workingDirectory = dir);
+  await runCmd(FlutterCmd(['test'])..workingDirectory = dir);
 }
 
 Future main() async {
